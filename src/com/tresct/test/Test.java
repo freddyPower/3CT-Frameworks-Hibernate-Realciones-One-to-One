93c9477d8d7a8ae543e758aef70c70d7d90da09c
Luis Alfredo Sanchez Angeles <lsanchez@MacBook-Air-de-Luis.local>
2017-09-03 21:39:14 -0500

    All

--- src/com/tresct/test/Test.java
+++ src/com/tresct/test/Test.java
@@ -10,7 +10,6 @@ import javax.persistence.criteria.Root;
 import org.hibernate.HibernateException;
 import org.hibernate.Session;
 
-
 import com.tresct.dto.Avaluo;
 import com.tresct.dto.Tramite;
 import com.tresct.util.HibernateUtil;
@@ -19,150 +18,142 @@ import com.tresct.util.HibernateUtil;
 public class Test {
 
 	public static void main(String[] args) {
-		
-		//Avaluo a = new Avaluo("Itzacalco");
-		//insertaAvaluo(a);
-		//actualizaAvaluo();
-		//consultaOneToOne();
+
 		try {
-			
+			// Avaluo a = new Avaluo("Itzacalco");
+			// insertaAvaluo(a);
+			// actualizaAvaluo();
+			// consultaOneToOne();
 			eliminaAvaluo();
-			
-		}catch( Exception hbe ) {
-			
+			// eliminaTramiteCascada();
+
+		} catch (Exception hbe) {
 			System.out.println("HibernateeXCEPTION: " + hbe.getMessage());
-			
 		}
-		
-		//eliminaTramiteCascada();
-		
 	}
-	
-	
-	//Metodo para insertar un avaluo
-	public static  void insertaAvaluo( Avaluo a) {
-		
+
+	// Metodo para insertar un avaluo
+	public static void insertaAvaluo(Avaluo a) {
+
 		Session sesion = null;
 		try {
-			
+
 			sesion = HibernateUtil.getSessionFactory().openSession();
 			sesion.beginTransaction();
-			
-			Tramite t = sesion.load(Tramite.class, 2);			
+
+			Tramite t = sesion.load(Tramite.class, 2);
 			System.out.println("El tramite cargado es: " + t.toString());
 			a.setTramite(t);
 			sesion.save(a);
 			sesion.getTransaction().commit();
-			
-		}catch(HibernateException  hbe) {			
-			System.out.println("HibernateException : " +  hbe.getMessage());
+
+		} catch (HibernateException hbe) {
+			System.out.println("HibernateException : " + hbe.getMessage());
 			sesion.getTransaction().rollback();
-			sesion.close();			
-		}finally {
-			sesion.close();			
+			sesion.close();
+		} finally {
+			sesion.close();
 		}
-		
+
 	}
-	
-	//Consulta one to one
-	public static String  consultaOneToOne() {
-		
+
+	// Consulta one to one
+	public static String consultaOneToOne() {
+
 		List<Avaluo> listaAvaluos = null;
 		Session sesion = null;
-				
+
 		try {
-		
+
 			sesion = HibernateUtil.getSessionFactory().openSession();
 			sesion.beginTransaction();
-			
-			//Consulta del tramite de un avaluo 
+
+			// Consulta del tramite de un avaluo
 			Avaluo a = sesion.load(Avaluo.class, 2);
 			Tramite t = a.getTramite();
-			
-			System.out.println("Tramite del avaluo 2: " + t.toString());			
-			
-			//Consulta de todos los tramites que aparecen en avaluos
+
+			System.out.println("Tramite del avaluo 2: " + t.toString());
+
+			// Consulta de todos los tramites que aparecen en avaluos
 			CriteriaBuilder builder = sesion.getCriteriaBuilder();
-			CriteriaQuery <Avaluo> criteria = builder.createQuery(Avaluo.class);
+			CriteriaQuery<Avaluo> criteria = builder.createQuery(Avaluo.class);
 			Root<Avaluo> root = criteria.from(Avaluo.class);
-			
+
 			criteria.select(root);
-			
+
 			listaAvaluos = sesion.createQuery(criteria).getResultList();
-			System.out.println("ListaAvaluos: "  + listaAvaluos.toString());
-			
-			
-		}catch( HibernateException hbe) {			
+			System.out.println("ListaAvaluos: " + listaAvaluos.toString());
+
+		} catch (HibernateException hbe) {
 			System.out.println("Excepcion en consultaOneToOne: " + hbe.getMessage());
 			sesion.getTransaction().rollback();
 			sesion.close();
-		}finally {
-			sesion.close();		
-		}			
-		return listaAvaluos.toString();		
+		} finally {
+			sesion.close();
+		}
+		return listaAvaluos.toString();
 	}
-	
-	
+
 	public static void actualizaAvaluo() {
-		
-		Session sesion =  null;
+
+		Session sesion = null;
 		try {
 			sesion = HibernateUtil.getSessionFactory().openSession();
 			sesion.beginTransaction();
-			
+
 			Avaluo a = sesion.load(Avaluo.class, 2);
 			a.setLugarAvaluo("2da cda de joya de nieves CDMX 2");
 			sesion.update(a);
 			sesion.getTransaction().commit();
-						
-		}catch( HibernateException hbe ) {
+
+		} catch (HibernateException hbe) {
 			System.out.println("HibernateException : " + hbe.getMessage());
 			sesion.getTransaction().rollback();
-			sesion.close();			
-		}finally {
-			sesion.close();			
-		}	
+			sesion.close();
+		} finally {
+			sesion.close();
+		}
 	}
-	
+
 	public static void eliminaAvaluo() {
 		Session sesion = null;
 		try {
 			sesion = HibernateUtil.getSessionFactory().openSession();
 			sesion.beginTransaction();
-			
+
 			Avaluo a = sesion.load(Avaluo.class, 3);
 			sesion.delete(a);
 			sesion.getTransaction().commit();
-			
-		}catch( HibernateException hbe ) {
+
+		} catch (HibernateException hbe) {
 			System.out.println("HibernateException: " + hbe.getMessage());
 			sesion.getTransaction().rollback();
 			sesion.close();
-			
-		}finally {
-			sesion.close();			
+
+		} finally {
+			sesion.close();
 		}
 	}
-	
-	//Eliminacion en cascada
+
+	// Eliminacion en cascada
 	public static void eliminaTramiteCascada() {
 		Session sesion = null;
 		try {
 			sesion = HibernateUtil.getSessionFactory().openSession();
 			sesion.beginTransaction();
 			Tramite t = sesion.load(Tramite.class, 2);
-			
+
 			sesion.delete(t);
 			sesion.getTransaction().commit();
-			
-		}catch( HibernateException hbe ) {
+
+		} catch (HibernateException hbe) {
 			System.out.println("HibernateException: " + hbe.getMessage());
 			sesion.getTransaction().rollback();
 			sesion.close();
-			
-		}finally {
-			sesion.close();			
+
+		} finally {
+			sesion.close();
 		}
 	}
-	
+
 }
